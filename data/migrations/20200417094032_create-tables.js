cd55b732654b6230994f073a119541e32538e936
Tyler Kummer <tylerkummer@protonmail.com>
2020-04-17 10:29:19 -0500

    Create Tables Migrations

--- data/migrations/20200417094032_create-tables.js
+++ data/migrations/20200417094032_create-tables.js
@@ -7,7 +7,45 @@ exports.up = function (knex) {
         tbl.string("name", 255).notNullable();
         tbl.text("description");
         tbl.boolean("completed").notNullable();
-        // resources
+      })
+      // resources
+      .createTable("resources", (tbl) => {
+        tbl.increments("id");
+        tbl.name("name", 255).notNullable();
+        tbl.text("description");
+      })
+      // projects_resources
+      .createTable("projects_resources", (tbl) => {
+        tbl
+          .integer("project_id")
+          .unsigned()
+          .notNullable()
+          .references("id")
+          .inTable("projects")
+          .onUpdate("CASCADE")
+          .onDelete("RESTRICT");
+        tbl
+          .integer("resource_id")
+          .unsigned()
+          .notNullable()
+          .references("id")
+          .inTable("resources")
+          .onUpdate("CASCADE")
+          .onDelete("RESTRICT");
+      })
+      .createTable("tasks", (tbl) => {
+        tbl.increments("id");
+        tbl.text("description").notNullable();
+        tbl.text("notes");
+        tbl.boolean("completed").notNullable();
+        tbl
+          .integer("project_id")
+          .unsigned()
+          .notNullable()
+          .references("id")
+          .inTable("projects")
+          .onUpdate("CASCADE")
+          .onDelete("RESTRICT");
       })
   );
 };
